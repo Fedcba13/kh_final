9e54101e98bdd0162ada8f0d4ffd61964b9d6938
KMH <klm8787@naver.com>
2019-10-09 15:26:48 +0900

    마이페이지 리뷰등록 완료.

--- UrbanTable/src/main/java/com/kh/urbantable/review/controller/ReviewController.java
+++ UrbanTable/src/main/java/com/kh/urbantable/review/controller/ReviewController.java
@@ -8,14 +8,14 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.urbantable.review.model.service.ReviewService;
 
-@Controller
+@RestController
+@RequestMapping("/member")
 public class ReviewController {
 	
 	Logger logger = LoggerFactory.getLogger(getClass());
@@ -23,8 +23,7 @@ public class ReviewController {
 	@Autowired
 	ReviewService reviewService;
 	
-	@ResponseBody
-	@RequestMapping("/member/writeReview")
+	@RequestMapping("/writeReview")
 	public Map<String, Object> writeReview(@RequestParam String detailNo,
 											@RequestParam String content,
 											HttpSession session){
@@ -42,6 +41,37 @@ public class ReviewController {
 		return resultMap;
 	}
 	
+	@RequestMapping("/modifyReview")
+	public Map<String, Object> modifyReview(@RequestParam String detailNo,
+											@RequestParam String content,
+											HttpSession session){
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		
+		Map<String,Object> param = new HashMap<String, Object>();
+		param.put("detailNo", detailNo);
+		param.put("content", content);
+		
+		int result = reviewService.updateReview(param);
+		
+		resultMap.put("msg", result>0 ? "수정 성공" : "수정 실패");
+		
+		return resultMap;
+	}
 	
-
+	@RequestMapping("/deleteReview")
+	public Map<String, Object> deleteReview(@RequestParam String detailNo,
+											HttpSession session){
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		
+		Map<String,Object> param = new HashMap<String, Object>();
+		param.put("detailNo", detailNo);
+		
+		int result = reviewService.deleteReview(param);
+		
+		resultMap.put("msg", result>0 ? "삭제 성공" : "삭제 실패");
+		
+		return resultMap;
+	}
 }
